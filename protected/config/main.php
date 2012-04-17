311d5634e0e3a28e1bdafa3a5412fa299400f107
doneitman <doneitman@gmail.com>
2012-04-17 22:51:43 +0530

    Update protected/config/main.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,7 +1,7 @@
 <?php
 
 // uncomment the following to define a path alias
-// Yii::setPathOfAlias('local','path/to/local-folder');
+ Yii::setPathOfAlias('local','path/to/local-folder');
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
